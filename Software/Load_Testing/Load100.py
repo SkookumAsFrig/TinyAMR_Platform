b85ba3ecd7f0cb7280c73f8c5734b9b34ce33f68
SkookumAsFrig <kss223@cornell.edu>
2020-01-02 10:21:49 -0500

    added load testing scripts

---
+++ Software/Load_Testing/Load100.py
@@ -0,0 +1,11 @@
+import time
+
+last_time = time.time()
+dur = 0
+gg = 1
+haha = 0.1
+
+while dur < 30:
+    dur = time.time()-last_time
+    haha = haha*400*1000/2102312043205 + 0.0005
+    gg = (haha + 10013410421) / gg + 0.1231421
